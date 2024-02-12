0bf3b2dee0382830529a1c6aade3bc601edf74bb
IceCobe <jaalaba@gmail.com>
2024-02-12 14:14:43 -0800

    Bank Churning Data Submitted

--- kaggleLibrary/judeKaggleLibrary.py
+++ kaggleLibrary/judeKaggleLibrary.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+import pandas as pd
 
 def load_data(file_name):
     # Initialize a SparkSession
@@ -9,3 +10,14 @@ def load_data(file_name):
     test_df = spark.read.csv(f'../data/{file_name}/test.csv', header=True, inferSchema=True)
 
     return train_df, test_df
+
+def create_csv(file_name, predictions):
+    # Assuming 'df' is your Spark DataFrame
+    # Convert the Spark DataFrame to a Pandas DataFrame
+    pandas_df = predictions.select("id", "prediction").toPandas()
+
+    # Define the local path where you want to save the CSV
+    output_csv_path = f"../data/{file_name}/submission.csv"
+
+    # Save the Pandas DataFrame as a CSV file
+    pandas_df.to_csv(output_csv_path, index=False)
